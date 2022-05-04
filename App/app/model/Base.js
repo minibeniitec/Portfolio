c3b91abe305763296ae89d03671cbe398b1fe518
Benjamin Garza <minibeniitec@gmail.com>
2022-05-03 20:30:52 -0700

    Sencha Cmd generated app using ExtJs 7.0.0.168

---
+++ App/app/model/Base.js
@@ -0,0 +1,7 @@
+Ext.define('Portfolio.model.Base', {
+    extend: 'Ext.data.Model',
+
+    schema: {
+        namespace: 'Portfolio.model'
+    }
+});
---
+++ App/app/model/Personnel.js
@@ -0,0 +1,7 @@
+Ext.define('Portfolio.model.Personnel', {
+    extend: 'Portfolio.model.Base',
+
+    fields: [
+        'name', 'email', 'phone'
+    ]
+});
