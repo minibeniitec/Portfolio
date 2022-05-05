cc3215e8bea057c8bb2f458579c557316d94e6e6
Benjamin Garza <minibeniitec@gmail.com>
2022-05-04 22:24:30 -0700

    Removed list class and example styling

--- App/classic/src/view/main/Main.js
+++ App/classic/src/view/main/Main.js
@@ -14,90 +14,27 @@ Ext.define('Portfolio.view.main.Main', {
         'Ext.window.MessageBox',
 
         'Portfolio.view.main.MainController',
-        'Portfolio.view.main.MainModel',
-        'Portfolio.view.main.List'
+        'Portfolio.view.main.MainModel'
     ],
+
+    title: 'Ben Garza',
+    iconCls: 'x-fa fa-user',
 
     controller: 'main',
     viewModel: 'main',
 
-    ui: 'navigation',
+    layout: 'fit',
 
-    tabBarHeaderPosition: 1,
-    titleRotation: 0,
+    tabPosition: 'left',
     tabRotation: 0,
 
-    header: {
-        layout: {
-            align: 'stretchmax'
-        },
-        title: {
-            bind: {
-                text: '{name}'
-            },
-            flex: 0
-        },
-        iconCls: 'fa-th-list'
-    },
-
-    tabBar: {
-        flex: 1,
-        layout: {
-            align: 'stretch',
-            overflowHandler: 'none'
-        }
-    },
-
-    responsiveConfig: {
-        tall: {
-            headerPosition: 'top'
-        },
-        wide: {
-            headerPosition: 'left'
-        }
-    },
-
-    defaults: {
-        bodyPadding: 20,
-        tabConfig: {
-            responsiveConfig: {
-                wide: {
-                    iconAlign: 'left',
-                    textAlign: 'left'
-                },
-                tall: {
-                    iconAlign: 'top',
-                    textAlign: 'center',
-                    width: 120
-                }
-            }
-        }
-    },
-
     items: [{
-        title: 'Home',
-        iconCls: 'fa-home',
-        // The following grid shares a store with the classic version's grid as well!
-        items: [{
-            xtype: 'mainlist'
-        }]
+        title: 'About Me'
     }, {
-        title: 'Users',
-        iconCls: 'fa-user',
-        bind: {
-            html: '{loremIpsum}'
-        }
+        title: 'Interests'
     }, {
-        title: 'Groups',
-        iconCls: 'fa-users',
-        bind: {
-            html: '{loremIpsum}'
-        }
+        title: 'Sample Projects'
     }, {
-        title: 'Settings',
-        iconCls: 'fa-cog',
-        bind: {
-            html: '{loremIpsum}'
-        }
+        title: 'Resume'
     }]
 });
